f7e65e874f8855b1ef21cfba6c34420f6b108541
rsabhi <abhi.rs@gmail.com>
2019-08-23 13:18:14 +1000

    :tada: first commit

---
+++ src/main/java/com/deltaa/superrduperr/SuperrduperrApplication.java
@@ -0,0 +1,13 @@
+package com.deltaa.superrduperr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperrduperrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperrduperrApplication.class, args);
+	}
+
+}
